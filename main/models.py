9352bea2ee80a33771b3aa74db2e8f5863027056
Timur1414 <63254053+Timur1414@users.noreply.github.com>
2022-08-23 16:17:54 +0300

    login + creating usersettings signal

--- main/models.py
+++ main/models.py
@@ -3,6 +3,8 @@
 """
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserSettings(models.Model):
@@ -11,3 +13,18 @@ class UserSettings(models.Model):
     """
     user = models.OneToOneField(to=get_user_model(), on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='images/users/', default='images/users/no_avatar.png')
+
+
+@receiver(post_save, sender=get_user_model())
+def update_profile_signal(sender, instance, created, **kwargs):  # pylint: disable=unused-argument
+    """
+    Обработчик создания юзера
+
+    :param sender: источник сигнала
+    :param instance: созданный объект
+    :param created: признак того, что объект был создан (или изменён)
+    :param kwargs: всё остальное
+    """
+    if not created:
+        return
+    UserSettings.objects.create(user=instance)
