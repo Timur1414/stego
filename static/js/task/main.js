fcd663380fc9d73ab483184a6352908fd541a4b1
Timur1414 <63254053+Timur1414@users.noreply.github.com>
2022-09-05 22:03:43 +0300

    refactor

--- static/js/task/main.js
+++ static/js/task/main.js
@@ -12,12 +12,10 @@ async function send_answer() {
     let response = await fetch(url + `api/check_answer/?id=${id}&answer=${user_answer}`)
     let data = await response.json()
     if (data.is_ok) {
-        console.log("yes")
         done_count.innerText = Number(done_count.innerText) + 1
         input_form.innerHTML = data.message
     }
     else {
-        console.log("no")
         if (!enable_message) {
             enable_message = true
             input_form.innerHTML = data.message + input_form.innerHTML
